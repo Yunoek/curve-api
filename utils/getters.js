1418aad527f06ee7d6a8b9c9cd1a75bbbc5f0b92
charlie-eth <charlie.allombert@gmail.com>
2021-03-09 16:56:37 +0000

    feat: get factory from registry

--- utils/getters.js
+++ utils/getters.js
@@ -4,7 +4,6 @@ import addressGetterAbi from '../constants/abis/address_getter.json';
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 const addressGetter = '0x0000000022d53366457f9d5e68ec105046fc4383'
 const multiCall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
-const factoryRegistry = '0x0959158b6040D32d04c301A72CBFD6b39E21c9AE'
 
 const getRegistry = memoize(async () => {
   const contract = new web3.eth.Contract(addressGetterAbi, addressGetter);
@@ -18,9 +17,13 @@ const getMultiCall = async () => {
   return multiCall
 }
 
-const getFactoryRegistry = async () => {
-  return factoryRegistry
-}
+const getFactoryRegistry = memoize(async () => {
+  const contract = new web3.eth.Contract(addressGetterAbi, addressGetter);
+  return contract.methods.get_address(3).call();
+}, {
+  promise: true,
+  maxAge: 10 * 60 * 1000, // 10 min
+});
 
 export {
   getRegistry,
