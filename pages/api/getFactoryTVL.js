f9421e7c6d28ce7aadfe4598194861e57d00ed19
charlie-eth <charlie.allombert@gmail.com>
2021-03-09 16:48:21 +0000

    feat: add factory tvl

--- pages/api/getFactoryTVL.js
+++ pages/api/getFactoryTVL.js
@@ -1,24 +1,28 @@
 import axios from 'axios';
 import Web3 from 'web3';
+import BigNumber from 'big-number';
+
 import { fn } from '../../utils/api';
-import registryAbi from '../../constants/abis/registry.json';
+import { getFactoryRegistry, getMultiCall } from '../../utils/getters';
+import registryAbi from '../../constants/abis/factory_registry.json';
+import multicallAbi from '../../constants/abis/multicall.json';
+import erc20Abi from '../../constants/abis/erc20.json';
 
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
-const registry_address = '0xF79D6aFBb6dA890132F9D7c355e3015f15F3406F';
 
 
 export default fn(async () => {
 
-
-  	let registry = new web3.eth.Contract(factory_registry_abi, factory_registry);
+    let registryAddress = await getFactoryRegistry()
+    let multicallAddress = await getMultiCall()
+  	let registry = new web3.eth.Contract(registryAbi, registryAddress);
   	let poolCount = await registry.methods.pool_count().call();
-  	let multicall = new web3.eth.Contract(multicall_abi, multicall_address)
+  	let multicall = new web3.eth.Contract(multicallAbi, multicallAddress)
 
-  	state.totalFactory = poolCount
   	//get pool addresses
   	let calls = []
   	for (var i = 0; i < poolCount; i++) {
-  		calls.push([factory_registry, registry.methods.pool_list(i).encodeABI()])
+  		calls.push([registryAddress, registry.methods.pool_list(i).encodeABI()])
   	}
   	let aggcalls = await multicall.methods.aggregate(calls).call()
   	let poolList = aggcalls[1].map(hex => web3.eth.abi.decodeParameter('address', hex))
@@ -28,31 +32,34 @@ export default fn(async () => {
   	poolList.map(async(pool_address) => {
   		calls.push([pool_address, '0xc66106570000000000000000000000000000000000000000000000000000000000000000']) //balances(0)
   	})
+
   	aggcalls = await multicall.methods.aggregate(calls).call()
   	let coinList = aggcalls[1].map(hex => web3.eth.abi.decodeParameter('address', hex))
+
+
 
   	//get decimals and balance
   	calls = []
   	coinList.map(async(coin_address, index) => {
-  		let erc20Contract = new web3.eth.Contract(ERC20_abi, coin_address)
+  		let erc20Contract = new web3.eth.Contract(erc20Abi, coin_address)
   		calls.push([coin_address, '0x313ce567']) //decimals
   		calls.push([coin_address, erc20Contract.methods.balanceOf(poolList[index]).encodeABI()]) //balance of pool
   	})
+
+
+
   	let balanceData = await multicall.methods.aggregate(calls).call()
   	balanceData = balanceData[1]
-
   	let factoryBalances = 0;
   	for (var i = 0; i < coinList.length * 2; i++) {
   		let decimals = web3.eth.abi.decodeParameter('uint8', balanceData[i])
   		i++
   		let balance = web3.eth.abi.decodeParameter('uint256', balanceData[i])
-  		factoryBalances += +BN(balance).div(10 ** decimals)
+  		factoryBalances += +BigNumber(balance).div(10 ** decimals)
   	}
 
-  	state.totalFactory = factoryBalances
-  	return factoryBalances
+    return { factoryBalances };
 
-  return { price };
 }, {
   maxAge: 20,
 });
--- utils/getters.js
+++ utils/getters.js
@@ -2,14 +2,24 @@ import Web3 from 'web3';
 import addressGetterAbi from '../constants/abis/address_getter.json';
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 const addressGetter = '0x0000000022d53366457f9d5e68ec105046fc4383'
-
+const multiCall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
+const factoryRegistry = '0x0959158b6040D32d04c301A72CBFD6b39E21c9AE'
 
 const getRegistry = async () => {
   let address_getter = new web3.eth.Contract(addressGetterAbi, addressGetter);
   return await address_getter.methods.get_registry().call();
 }
 
+const getFactoryRegistry = async () => {
+  return factoryRegistry;
+}
+
+const getMultiCall = async() => {
+  return multiCall;
+}
 
 export {
   getRegistry,
+  getMultiCall,
+  getFactoryRegistry
 };
