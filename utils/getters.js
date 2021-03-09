e721760a8f2b55168de2c98af5e38c465b9e6e8c
charlie-eth <charlie.allombert@gmail.com>
2021-03-09 19:07:14 +0000

    add weekly fee tables

--- utils/getters.js
+++ utils/getters.js
@@ -4,6 +4,8 @@ import addressGetterAbi from '../constants/abis/address_getter.json';
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 const addressGetter = '0x0000000022d53366457f9d5e68ec105046fc4383'
 const multiCall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
+const feeDistributor = '0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc'
+
 
 const getRegistry = memoize(async () => {
   const contract = new web3.eth.Contract(addressGetterAbi, addressGetter);
@@ -25,8 +27,15 @@ const getFactoryRegistry = memoize(async () => {
   maxAge: 10 * 60 * 1000, // 10 min
 });
 
+const getFeeDistributor = async () => {
+  return feeDistributor
+}
+
+
+
 export {
-  getRegistry,
+  getFactoryRegistry,
+  getFeeDistributor,
   getMultiCall,
-  getFactoryRegistry
+  getRegistry
 };
