108f25537c7ad89ddecf8599d99170925f816a9c
Philippe Masset <pioul06+github@gmail.com>
2021-08-20 16:50:16 +0200

    Add v1 facto meta implementation addresses to mapping

--- pages/api/getFactoryV2Pools.js
+++ pages/api/getFactoryV2Pools.js
@@ -10,22 +10,24 @@ import getTokensPrices from '../../utils/data/tokens-prices';
 const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 
 const implementationAddressMap = new Map([
-  ['0x6523Ac15EC152Cb70a334230F6c5d62C5Bd963f1', 'plain2basic'],
-  ['0x24D937143d3F5cF04c72bA112735151A8CAE2262', 'plain2balances'],
-  ['0x6326DEbBAa15bCFE603d831e7D75f4fc10d9B43E', 'plain2eth'],
-  ['0x4A4d7868390EF5CaC51cDA262888f34bD3025C3F', 'plain2optimized'],
-  ['0x9B52F13DF69D79Ec5aAB6D1aCe3157d29B409cC3', 'plain3basic'],
-  ['0x50b085f2e5958C4A87baf93A8AB79F6bec068494', 'plain3balances'],
-  ['0x8c1aB78601c259E1B43F19816923609dC7d7de9B', 'plain3eth'],
-  ['0xE5F4b89E0A16578B3e0e7581327BDb4C712E44De', 'plain3optimized'],
-  ['0x5Bd47eA4494e0F8DE6e3Ca10F1c05F55b72466B8', 'plain4basic'],
-  ['0xd35B58386705CE75CE6d09842E38E9BE9CDe5bF6', 'plain4balances'],
-  ['0x88855cdF2b0A8413D470B86952E726684de915be', 'plain4eth'],
-  ['0xaD4753D045D3Aed5C1a6606dFb6a7D7AD67C1Ad7', 'plain4optimized'],
-  ['0x213be373FDff327658139C7df330817DAD2d5bBE', 'metausd'],
-  ['0x55aa9bf126bcabf0bdc17fa9e39ec9239e1ce7a9', 'metausdbalances'],
-  ['0xC6A8466d128Fbfd34AdA64a9FFFce325D57C9a52', 'metabtc'],
-  ['0xc4C78b08fA0c3d0a312605634461A88184Ecd630', 'metabtcbalances'],
+  ['0x6523Ac15EC152Cb70a334230F6c5d62C5Bd963f1'.toLowerCase(), 'plain2basic'],
+  ['0x24D937143d3F5cF04c72bA112735151A8CAE2262'.toLowerCase(), 'plain2balances'],
+  ['0x6326DEbBAa15bCFE603d831e7D75f4fc10d9B43E'.toLowerCase(), 'plain2eth'],
+  ['0x4A4d7868390EF5CaC51cDA262888f34bD3025C3F'.toLowerCase(), 'plain2optimized'],
+  ['0x9B52F13DF69D79Ec5aAB6D1aCe3157d29B409cC3'.toLowerCase(), 'plain3basic'],
+  ['0x50b085f2e5958C4A87baf93A8AB79F6bec068494'.toLowerCase(), 'plain3balances'],
+  ['0x8c1aB78601c259E1B43F19816923609dC7d7de9B'.toLowerCase(), 'plain3eth'],
+  ['0xE5F4b89E0A16578B3e0e7581327BDb4C712E44De'.toLowerCase(), 'plain3optimized'],
+  ['0x5Bd47eA4494e0F8DE6e3Ca10F1c05F55b72466B8'.toLowerCase(), 'plain4basic'],
+  ['0xd35B58386705CE75CE6d09842E38E9BE9CDe5bF6'.toLowerCase(), 'plain4balances'],
+  ['0x88855cdF2b0A8413D470B86952E726684de915be'.toLowerCase(), 'plain4eth'],
+  ['0xaD4753D045D3Aed5C1a6606dFb6a7D7AD67C1Ad7'.toLowerCase(), 'plain4optimized'],
+  ['0x213be373FDff327658139C7df330817DAD2d5bBE'.toLowerCase(), 'metausd'],
+  ['0x55aa9bf126bcabf0bdc17fa9e39ec9239e1ce7a9'.toLowerCase(), 'metausdbalances'],
+  ['0xC6A8466d128Fbfd34AdA64a9FFFce325D57C9a52'.toLowerCase(), 'metabtc'],
+  ['0xc4C78b08fA0c3d0a312605634461A88184Ecd630'.toLowerCase(), 'metabtcbalances'],
+  ['0x5F890841f657d90E081bAbdB532A05996Af79Fe6'.toLowerCase(), 'v1metausd'],
+  ['0x2f956eee002b0debd468cf2e0490d1aec65e027f'.toLowerCase(), 'v1metabtc'],
 ]);
 
 const assetTypeMap = new Map([
@@ -150,7 +152,7 @@ export default fn(async () => {
   }, emptyData);
 
   const augmentedData = mergedPoolData.map((poolInfo) => {
-    const implementation = implementationAddressMap.get(poolInfo.implementationAddress);
+    const implementation = implementationAddressMap.get(poolInfo.implementationAddress.toLowerCase());
     const assetTypeName = assetTypeMap.get(poolInfo.assetType);
 
     const coins = poolInfo.coinsAddresses
