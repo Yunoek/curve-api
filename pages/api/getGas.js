5a325939ae181565848c7d7190eb49cc67a2f46c
Philippe Masset <pioul06+github@gmail.com>
2021-08-23 12:36:12 +0200

    Add eip1559 gas calcs to getGas endpoint

--- pages/api/getGas.js
+++ pages/api/getGas.js
@@ -1,15 +1,56 @@
-import axios from 'axios';
-import Web3 from 'web3';
+import memoize from 'memoizee';
 import { fn } from '../../utils/api';
-import aggregatorInterfaceABI from '../../constants/abis/aggregator.json';
 
-const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
-const chainlinkETHUSDaddress = '0xF79D6aFBb6dA890132F9D7c355e3015f15F3406F';
+const getEtherscanData = memoize(async () => {
+  const { result: { SafeGasPrice, ProposeGasPrice, FastGasPrice } } =
+    await (await fetch('https://api.etherscan.io/api?module=gastracker&action=gasoracle')).json();
+
+  return [
+    parseInt(FastGasPrice, 10),
+    parseInt(ProposeGasPrice, 10),
+    parseInt(SafeGasPrice, 10),
+  ];
+}, {
+  promise: true,
+  maxAge: 15 * 1000, // Price is refreshed every 15s
+});
+
+const getGasNowData = memoize(async () => {
+  const { data } = await (await fetch('https://www.gasnow.org/api/v3/gas/price?utm_source=curve')).json();
+
+  return data; // { rapid, fast, standard, slow, timestamp }
+}, {
+  promise: true,
+  maxAge: 15 * 1000, // Price is refreshed every 15s
+});
 
 export default fn(async () => {
-  let data = await axios.get('https://www.gasnow.org/api/v3/gas/price?utm_source=curve')
-  let gas = data.data.data
-  return { gas };
+  const [gasNowData, etherscanData] = await Promise.all([
+    getGasNowData(),
+    getEtherscanData(),
+  ]);
+
+  const baseFee = etherscanData[2] * 1e9;
+  const baseFeePaddingFactor = 4 * 0.125; // Enough to allow base fee to go up in 4 consecutive blocks
+
+  const gasNowPrices = [gasNowData.rapid, gasNowData.fast, gasNowData.standard, gasNowData.slow];
+  const minPrioPrice = [4, 3, 2, 1];
+  const suggestedPrioPrices = gasNowPrices.map((totalPrice, i) => (
+    Math.max(parseInt(totalPrice, 10) - baseFee, minPrioPrice[i])
+  ));
+
+  const eip1559Gas = {
+    base: baseFee,
+    prio: suggestedPrioPrices,
+    max: suggestedPrioPrices.map((suggestedPrioPrice) => (
+      baseFee + (baseFee * baseFeePaddingFactor) + suggestedPrioPrice
+    )),
+  };
+
+  return {
+    gas: gasNowData,
+    eip1559Gas,
+  };
 }, {
   maxAge: 30,
 });
