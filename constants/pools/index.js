79f48ef11767b021dff7120e4573d57564d3f172
Yunoek <Yunoek@pm.me>
2021-10-20 15:13:07 +0200

    ♻️ Moving back to dollar?

--- constants/pools/index.js
+++ constants/pools/index.js
@@ -15,7 +15,7 @@ const defaultPoolTemplate = {
 	coingeckoInfo: {
 		id: undefined, // Must be defined
 		symbol: undefined, // Must be defined
-		referenceAssetId: 'usd',
+		referenceAssetId: 'dollar',
 		referenceVsId: 'usd',
 	},
 	assets: undefined,
