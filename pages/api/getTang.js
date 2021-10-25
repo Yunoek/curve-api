193fb776eed578b301cb36dc71e8d9e6a2f5551b
Yunoek <Yunoek@pm.me>
2021-10-25 18:47:38 +0200

    ✨ Updating API with supploes

--- pages/api/getTang.js
+++ pages/api/getTang.js
@@ -13,6 +13,8 @@ const STACKER = '0x989AEb4d175e16225E39E87d0D97A3360524AD80';
 const LENS = '0x83d95e0D5f402511dB06817Aff3f9eA88224B030'; // to get 3crypto price
 const CONVEX_BOOSTER = '0xF403C135812408BFbE8713b5A23a04b3D48AAE31';
 const TANG_BOOSTER = '0xF403C135812408BFbE8713b5A23a04b3D48AAE31';
+const CRV_REWARD_ADDR = '0x3Fe65692bfCD0e6CF84cB1E7d24108E434A7587e';
+const CONVEX_ADDR = '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B';
 
 async function	getTangAndConvex() {
 	const provider = new ethers.providers.AlchemyProvider('homestead', process.env.ALCHEMY_API_KEY);
@@ -39,6 +41,12 @@ async function	getTangAndConvex() {
 	const	tangBoosterContractMulti = new Contract(TANG_BOOSTER, ['function poolInfo(uint256) public view returns (address, address, address, address, address, bool)']);
 
 	/**********************************************************************
+	**	And then it would be nice to have the supply of CRV available
+	**********************************************************************/
+	const	tangCRVSupply = new Contract(CRV_REWARD_ADDR, ['function totalSupply() public view returns (uint256)']);
+	const	convexSupply = new Contract(CONVEX_ADDR, ['function totalSupply() public view returns (uint256)']);
+
+	/**********************************************************************
 	**	Once we have the number of pools, we can get the info of each pool
 	**	with a multicall process. They will be stored in an array and will
 	**	follow this structure :
@@ -62,20 +70,23 @@ async function	getTangAndConvex() {
 	for (let index = 0; index < cvxPoolLength; index++) {
 		preparedCall.push(convexBoosterContractMulti.poolInfo(index));
 	}
+	preparedCall.push(tangCRVSupply.totalSupply());
+	preparedCall.push(convexSupply.totalSupply());
 	await	ethcallProvider.init();
 	const	allPools = await ethcallProvider.all(preparedCall);
 	const	tangPools = allPools.slice(0, tangPoolLength);
 	const	cvxPools = allPools.slice(tangPoolLength, tangPoolLength + cvxPoolLength);
+	const	rest = allPools.slice(tangPoolLength + cvxPoolLength);
 
 	/**********************************************************************
 	**	Finally, we can populate the tangAddress object
 	**********************************************************************/
-	const	tangAddress = {};
+	const	tang = {};
 	for (let index = 0; index < tangPools.length; index++) {
 		const	poolId = pools.find(e => (e.addresses?.lpToken).toLowerCase() === (tangPools[index][0]).toLowerCase())?.id;
 		if (poolId) {
-			tangAddress[poolId] = {
-				...tangAddress[poolId],
+			tang[poolId] = {
+				...tang[poolId],
 				tangToken: tangPools[index][1],
 				tangRewards: tangPools[index][3],
 			};
@@ -85,15 +96,20 @@ async function	getTangAndConvex() {
 	for (let index = 0; index < cvxPools.length; index++) {
 		const	poolId = pools.find(e => (e.addresses?.lpToken).toLowerCase() === (cvxPools[index][0]).toLowerCase())?.id;
 		if (poolId) {
-			tangAddress[poolId] = {
-				...tangAddress[poolId],
+			tang[poolId] = {
+				...tang[poolId],
 				cvxToken: cvxPools[index][1],
 				cvxRewards: cvxPools[index][3],
 			};
 		}
 	}
 	
-	return tangAddress;
+	const	supply = {
+		tangCRV: ethers.utils.formatEther(rest[0]),
+		convex: ethers.utils.formatEther(rest[1]),
+	};
+
+	return {tang, supply};
 }
 
 async function getTVL() {
@@ -164,7 +180,7 @@ export default fn(async ({address}) => {
 			stackedTangAPYs
 		},
 		{tvl, vsPrices},
-		tangAndConvex,
+		{tang, supply},
 	] = await Promise.all([
 		getCurveRewards(),
 		getAPY(),
@@ -173,10 +189,9 @@ export default fn(async ({address}) => {
 		getTangAndConvex(),
 	]);
 
-	return {stackedTangAPYs, pools: arrayToHashmap(pools.map((pool, index) => [pool.id, {
+	return {stackedTangAPYs, supplies: supply, pools: arrayToHashmap(pools.map((pool, index) => [pool.id, {
 		id: pool.id,
 		name: pool.name,
-		poolName: pool.poolName,
 		lpName: pool.lpName,
 		cgID: pool.coingeckoInfo?.id,
 		addresses: {
@@ -185,7 +200,7 @@ export default fn(async ({address}) => {
 			gauge: pool.addresses.gauge,
 			deposit: pool.addresses.deposit,
 			migrator: pool.addresses.migrator,
-			tang: tangAndConvex?.[pool.id] || {},
+			tang: tang?.[pool.id] || {},
 		},
 		baseApy: baseApys[index],
 		crvApy: crvApys[pool.id],
